9f881d571bf4aa923324a4ef775bd7002631f6b3
miyazaki-kotone <cotton.owo28@gmail.com>
2025-04-26 23:09:28 +0900

    テーブルの長さ変更。ゴールを自動的にテーブルの端っこにつけるよう対応
    Signed-off-by: miyazaki-kotone <cotton.owo28@gmail.com>

--- ExcellentTeamGames/Assets/Scenes/MainGame/Scripts/GoalObj.cs
+++ ExcellentTeamGames/Assets/Scenes/MainGame/Scripts/GoalObj.cs
@@ -5,6 +5,20 @@ namespace MiyazakiScript
 {
     public class GoalObj : MonoBehaviour
     {
+        [SerializeField] private GameObject table; // 右端を取りたいオブジェクト
+
+        void Start()
+        {
+            var collider = table.GetComponent<Collider>();
+            // 右端のX座標を取得
+            float rightEdgeX = collider.bounds.max.x;
+
+            // 自分のポジションを更新
+            Vector3 myPos = transform.position;
+            myPos.x = rightEdgeX;
+            transform.position = myPos;
+        }
+
         private void OnTriggerEnter(Collider other)
         {
             if (other.CompareTag("Player"))
