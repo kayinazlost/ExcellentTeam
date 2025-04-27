44b0250bc17395097a4a51c5788e5580fcb91819
マスタープロジェクトフェンリル <masterprojectfenrir@gmail.com>
2025-04-27 13:26:31 +0900

    更新

---
+++ ExcellentTeamGames/Assets/Test/Inoue/Script/DoorSystem.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.PackageManager.UI;
+using UnityEngine;
+
+public class DoorSystem : MonoBehaviour
+{
+    // --- インスタンス保持用のstatic変数 ---
+    public static DoorSystem instance;
+
+    // --- 変更対象のフラグ ---
+    public bool m_OpenFlag;
+
+    public Animator m_Animator;
+
+    void Awake()
+    {
+        m_Animator = GetComponent<Animator>();
+        // --- 起動時に自身をstatic変数に代入しておく ---
+        instance = this;
+    }
+
+    // --- static関数でm_OpenFlagを変更できるようにする ---
+    public static void SetOpenFlag(bool flag)
+    {
+        if (instance != null)
+        {
+            instance.m_OpenFlag = flag;
+
+            instance.m_Animator.SetBool("開く", flag);
+        }
+    }
+}
