9fedc4c7f5014acf184e5d17a4c8010358ea1d17
miyazaki-kotone <cotton.owo28@gmail.com>
2025-04-27 13:27:17 +0900

    タイマー変更

--- ExcellentTeamGames/Assets/Scenes/MainGame/Scripts/GameManagaer.cs
+++ ExcellentTeamGames/Assets/Scenes/MainGame/Scripts/GameManagaer.cs
@@ -19,6 +19,7 @@ public class GameManager : MonoBehaviour
 
     public float elapsedTime = 0f;
     private bool isGameRunning = true;
+    public float PlayTime = 0f;
 
     private void Awake()
     {
@@ -38,7 +39,18 @@ public class GameManager : MonoBehaviour
         if (isGameRunning)
         {
             elapsedTime += Time.deltaTime;
+            PlayTime += Time.deltaTime;
         }
+    }
+
+    public void Kaihuku()
+    {
+        elapsedTime -= 10f;
+        if (elapsedTime < 0f)
+        {
+            elapsedTime = 0;
+        }
+
     }
 
     public float GetElapsedTime()
